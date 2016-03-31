4f45407d15e7480503d2b18bac4ee737083c66f3
Mark Neumann <markneumann@gmail.com>
2016-03-31 10:23:56 -0700

    currently the find is broken for index path

--- server/app/controller/boardstate_controller.js
+++ server/app/controller/boardstate_controller.js
@@ -1,7 +1,7 @@
 // BoardState Controller
 console.log("loading boardstate_controller");
 var mongoose = require('mongoose');
-var BoardState = mongoose.model('boardstate');
+var BoardStates = mongoose.model('boardstates');
 var catch_errors = function(err){
     res.json({error:err});
 };
@@ -12,14 +12,16 @@ module.exports = (function() {
             //console.log(req.params);
             // search for field combined of username1 + vs + username2
             //admiralVsadmiral = {"ava": req.params.ava};
+            console.log('BoardStateSchema = ', BoardStates);
             console.log('ava =', req.params.ava);
-            BoardState.find({"ava": req.params.ava}, function(err, boardstate) {
+        //    BoardState.find({"ava": req.params.ava}, function(err, theboard) {
+            BoardStates.find({}, function(err, theboard) {
                 if(err) {
                     console.log(err);
                     //res.render('errors', {title: 'you have errors!', errors: boardstate.err});
                 } else {
-                    console.log('boardstate =',boardstate);
-                    res.json(boardstate); //<-- think we change this
+                    console.log('boardstate =',theboard);
+                    res.json(theboard); //<-- think we change this
                 }
             });
         },
@@ -51,30 +53,30 @@ module.exports = (function() {
         //  When ships are placed on the board (set to ship value), and
         //  When shots are fired (hit or miss)
         //  Requestor should provide the _id of the position
-        edit_boardstate: function(req, res) {
-            console.log("--> edit boardstate path");
-            console.log("req.body =", req.body);
-            var update = {};
-            var pos_id = {_id: req.body.pos_id};
-            console.log('opt_num type=', typeof(req.body.opt_num));
-            var updateStr = '{$inc : {"option'+req.body.opt_num +'count" : 1}}';
-            //update = {$inc : {'option'+req.body.opt _num+'count' : 1 }};
-            console.log('update =', update);
-            console.log('updateStr =', updateStr);
-            console.log('q_id = ',q_id);
-            BoardState.findByIdAndUpdate(q_id,update)
-            .then(function() {
-                console.log("return 200");
-                res.status(200); // send back http 200 status if successful
-                res.json({success:'true'});
-            })
-            .catch (function(err){
-                console.log(err);
-                res.status(500); // send back http 200 status if successful
-                res.json({error: err});
-            });
-        },
-//
+//         edit_boardstate: function(req, res) {
+//             console.log("--> edit boardstate path");
+//             console.log("req.body =", req.body);
+//             var update = {};
+//             var pos_id = {_id: req.body.pos_id};
+//             console.log('opt_num type=', typeof(req.body.opt_num));
+//             var updateStr = '{$inc : {"option'+req.body.opt_num +'count" : 1}}';
+//             //update = {$inc : {'option'+req.body.opt _num+'count' : 1 }};
+//             console.log('update =', update);
+//             console.log('updateStr =', updateStr);
+//             console.log('q_id = ',q_id);
+//             BoardState.findByIdAndUpdate(q_id,update)
+//             .then(function() {
+//                 console.log("return 200");
+//                 res.status(200); // send back http 200 status if successful
+//                 res.json({success:'true'});
+//             })
+//             .catch (function(err){
+//                 console.log(err);
+//                 res.status(500); // send back http 200 status if successful
+//                 res.json({error: err});
+//             });
+//         },
+// //
 //         // remove_question:  function(req, res){
 //         //     console.log("--> remove question path");
 //         //     console.log(req.params);
