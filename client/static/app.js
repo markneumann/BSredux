ea31c9ea996608b204b0e0e75c431fd50432f181
gnoevil <geno.guerrero@gmail.com>
2016-04-01 12:06:46 -0700

    APR1-updates

--- client/static/app.js
+++ client/static/app.js
@@ -3,19 +3,29 @@ console.log('loading app.js');
 
 // had to comment out ngRoute to make the plain arrays load
 // var BSModule = angular.module('app', ['ngRoute']);
-var BSModule = angular.module('app', []);
+var BSModule = angular.module('app', ['ngRoute']);
+
+.controller('BoardController1', function($scope, $routeParams) {
+    $scope.name = "BookController1";
+    $scope.params = $routeParams;
+})
+
+.controller('BoardController2', function($scope, $routeParams) {
+    $scope.name = "BoardController2";
+    $scope.params = $routeParams;
+})
 
 console.log('BSModule = ', BSModule);
 // the .controller() method adds a controller to the module
-// BSModule.config(function($routeProvider){
-//     $routeProvider
-//     .when('/', {
-//         templateUrl: '/partials/board.html'
-//     })
-//     // .when('/customers', {
-//     //     templateUrl: '/partials/customers.html'
-//     // })
-//     .otherwise({
-//         redirectTo: '/'
-//     });
-// });
+BSModule.config(function($routeProvider){
+    $routeProvider
+    .when('/user1', {
+        templateUrl: '/partials/left_gameboard.html'
+    })
+    .when('/user2', {
+        templateUrl: 'partials/right_gameboard.html'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+});
